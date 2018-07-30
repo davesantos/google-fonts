8138a6e5236c7536bf6a0c9859d3319aacff3ab0
davesantos <info@davesantos.com>
2018-07-29 18:57:06 -0700

    Update to gulp 4.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-var gulp = require('gulp'),
-	browserSync = require('browser-sync'),
-	changed = require('gulp-changed'),
-	cleanCSS = require('gulp-clean-css'),
-	sass = require('gulp-sass'),
- 	pug = require('gulp-pug'),
-	prettify = require('gulp-prettify');
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var changed = require('gulp-changed');
+var cleanCSS = require('gulp-clean-css');
+var prettify = require('gulp-prettify');
+var pug = require('gulp-pug');
+var sass = require('gulp-sass');
 
 var paths = {
   sass: '_sass',
@@ -17,58 +17,59 @@ function errorHandler(error) {
 	browserSync.notify('Error');
 }
 
+
 gulp.task('sass', function(){
-	gulp.src(paths.sass + '/**/*.{sass,scss}')
-		.pipe(sass().on('error', errorHandler))
-		.pipe( cleanCSS({
-		  debug: true,
-		  keepBreaks: true,
-		  keepSpecialComments: false
-		}, function(details) {
-		  console.log(details.name + ': ' + details.stats.originalSize);
-		  console.log(details.name + ': ' + details.stats.minifiedSize);
-		}) )
-		.pipe(gulp.dest(paths.css))
-		.pipe(browserSync.reload({stream:true}))
+  return gulp.src(paths.sass + '/**/*.{sass,scss}')
+    .pipe(sass().on('error', errorHandler))
+    .pipe( cleanCSS({
+      debug: true,
+      keepBreaks: true,
+      keepSpecialComments: false
+    }, function(details) {
+      console.log(details.name + ': ' + details.stats.originalSize + ' ==> ' + details.stats.minifiedSize);
+    }) )
+    .pipe(gulp.dest(paths.css))
+    .pipe(browserSync.reload({stream:true}))
 });
 
 gulp.task('pug', function(){
-	gulp.src('./*.pug')
-		.pipe(changed('.', {extension: '.html'}))
-		.pipe(pug({
-			pretty: true
-		}))
-		.pipe(gulp.dest('.'));
-		browserSync.reload();
-})
-
-
-gulp.task('indent', function(){
-	gulp.src('*.html')
-		.pipe(prettify({
-			indent_inner_html: true,
-			indent_with_tabs: true,
-			indent_char:"	",
-			indent_size: 1,
-			preserve_newlines: false
-		}))
-		.pipe(gulp.dest('.'));
+  return gulp.src('./*.pug')
+    .pipe(changed('.', {extension: '.html'}))
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('.'));
 });
 
+gulp.task('indent', function(){
+  return gulp.src('*.html')
+    .pipe(prettify({
+      indent_inner_html: true,
+      indent_with_tabs: true,
+      indent_char:" ",
+      indent_size: 1,
+      preserve_newlines: false
+    }))
+    .pipe(gulp.dest('.'));
+});
 
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', gulp.series('sass', function() {
 
   browserSync.init({
-	  server: {
+    server: {
       baseDir: "./"
     }
   });
 
-	gulp.watch( paths.sass + '/**/*.{sass,scss}', ['sass']);
-	gulp.watch('./*.pug', ['pug']);
-	gulp.watch(['./js/*']).on('change', browserSync.reload );
+  gulp.watch(paths.sass + '/**/*.{sass,scss}', gulp.parallel('sass')).on('change', browserSync.reload );
+  gulp.watch('./*.pug', gulp.parallel('pug')).on('change', browserSync.reload );
+  gulp.watch(['./js/*']).on('change', browserSync.reload );
 
-})
+}));
 
-gulp.task('default', ['serve']);
+gulp.task('travis', gulp.series(gulp.parallel('pug', 'indent', 'serve'), function() {
+  console.log('complete');
+}));
+
+gulp.task('default', gulp.series('serve'));
 
